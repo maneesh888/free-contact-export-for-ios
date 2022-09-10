629d9af5c531fbb17e54fc4d5bf22279a91d3549
Maneesh <maneesh888@gmail.com>
2022-09-10 06:21:29 +0400

    First commit - project created

---
+++ Free-Contact-Export/Free-Contact-Export/Free_Contact_ExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Free_Contact_ExportApp.swift
+//  Free-Contact-Export
+//
+//  Created by Maneesh M on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Free_Contact_ExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
